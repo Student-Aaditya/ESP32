bbf30648a32019cf0560931998cab1212a9935c0
Student-Aaditya <aadityakuamr518@gmail.com>
2026-06-07 12:10:05 +0530

    Adding listen server

--- server.js
+++ server.js
@@ -13,3 +13,19 @@ app.get("/", (req, res) => {
     gpioState = "HIGH"; console.log("GPIO -> HIGH");
      res.json({ success: true, state: gpioState }); 
     });
+
+    // Turn OFF 
+    app.post("/gpio/off", (req, res) => { 
+      gpioState = "LOW"; console.log("GPIO -> LOW");
+       res.json({ success: true, state: gpioState });
+       });
+
+       // Get Status 
+app.get("/gpio/status", (req, res) => { 
+   res.json({ state: gpioState }); 
+});
+
+const PORT = process.env.PORT || 3000;
+ app.listen(PORT, () => { 
+   console.log(`Server running on port ${PORT}`); 
+});
